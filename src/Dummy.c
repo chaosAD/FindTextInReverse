890447fabff916e9a5f9442eb263a0f4c0aa8ff4
Twinkle <twinkle0613@live.com>
2015-06-03 12:02:31 +0800

    asd

--- src/Dummy.c
+++ src/Dummy.c
@@ -8,13 +8,13 @@ int doSomeOtherStuffs() {
   return -1;
 }
 
-<<<<<<< HEAD
+
 int doSomestherStuffs() {
   return 0;
 }
-=======
+
 int whatToDoToStopAssignment()
 {
   return 0;
 }
->>>>>>> cb18381f06b3536899ff4f6fd823129f734108a8
+
